38cbf16898058cc43cb24a096cbad86c27c04619
promowar <56930854+promowar@users.noreply.github.com>
2024-08-08 15:10:20 +0300

    Add files via upload

---
+++ moduel_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls = calls + 1
+    return calls
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    a = string.lower()
+    b = [x.lower() for x in list_to_search]
+    if a in b:
+        return True
+    else:
+        return False
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
